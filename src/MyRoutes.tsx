929f32fea8f91dbd49660ae81bceb2f46334c1f0
Abdugaffarov2002 <pulat.96kg@gmail.com>
2023-08-05 13:55:29 +0600

    navbar

---
+++ src/MyRoutes.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyRoutes = () => {
+  return <div>MyRoutes</div>;
+};
+
+export default MyRoutes;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+  let navigate = useNavigate();
+  return (
+    <div>
+      <Button onClick={() => navigate("/")}>Home</Button>
+      <Button onClick={() => navigate("/catalog")}>Catalog</Button>
+      <Button onClick={() => navigate("/add")}>Add</Button>
+    </div>
+  );
+};
+
+export default Navbar;
